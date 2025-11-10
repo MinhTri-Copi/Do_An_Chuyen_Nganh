26240265d107e4a7641a70ab8a4a3973a1bd99aa
DuongPhamMinhTri <minhtri24902004@gmail.com>
2025-11-10 23:07:57 +0700

    Setup db for all user

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ viewEngineConfig(app);
 initWebRoutes(app);
 
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8083;
 
 // Test database connection và start server
 const startServer = async () => {
